be6864ba92df72c3081c0eea98c6f381cf97191d
Silence1354 <igor2406b@gmail.com>
2022-05-08 12:34:57 +0300

    Initial

---
+++ gatsby-config.ts
@@ -0,0 +1,18 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-graphcms',
+      options: {
+        endpoint: "https://api-ca-central-1.graphcms.com/v2/cl2kmw2o40ni501z46usg4te0/master",
+      },
+    },
+    'gatsby-plugin-image',
+  ],
+}
+
+export default config
